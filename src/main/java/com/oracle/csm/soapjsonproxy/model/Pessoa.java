46ee59a38e1040c47f28db7c759a03373f9a766b
rtenorio <rodrigo.tenorio@oracle.com>
2015-09-27 12:39:59 -0300

    Fisrt commit.

---
+++ src/main/java/com/oracle/csm/soapjsonproxy/model/Pessoa.java
@@ -0,0 +1,36 @@
+package com.oracle.csm.soapjsonproxy.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Pessoa {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(final String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(final String cpf) {
+        this.cpf = cpf;
+    }
+}
